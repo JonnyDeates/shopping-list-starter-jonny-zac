a1a4f319eb2ac95841c4c0a062aca98bef24e3b7
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2019-09-25 09:45:39 -0600

    Update 3

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,8 @@ import item from './item.js';
 import store from './store.js';
 
 const main = function () {
-    const itemNames = [ '', 'apples', 'pears' ];
-    itemNames.forEach(name => {
-        try {
-            item.validateName(name);
-            // create a new item if name is valid
-            store.items.push(item.create(name));
-        } catch(error) {
-            console.log(`Cannot add item: ${error.message}`);
-        }
-    });
+
+    store.findAndDelete(id);
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
--- scripts/store.js
+++ scripts/store.js
@@ -3,4 +3,38 @@ import item from './item.js';
 const items = [];
 const hideCheckedItems = false;
 
-export default {items, hideCheckedItems};
+
+function findById(id) {
+    return items.find(item => item.id === id);
+}
+
+function addItem(itemName) {
+    try {
+        item.validateName(itemName);
+        this.items.push(item.create(itemName));
+    } catch (error) {
+        console.log(`Cannot add item: ${error.message}`);
+    }
+}
+
+function findAndToggleChecked(id){
+    let item = findById(id);
+    item.checked = !item.checked;
+}
+function findAndUpdateName(id, newName) {
+    try {
+        item.validateName(newName);
+        let item = this.findById(id)
+        item.name = newName;
+    } catch (error) {
+        console.log(`Cannot update name: ${error.message}`);
+    }
+}
+
+function findAndDelete(id) {
+    if (!(!!this.items.findIndex((item) => item.id === id))) {
+        this.items.splice(this.items.findIndex((item) => item.id === id), 1);
+    }
+}
+
+export default {items, hideCheckedItems, addItem, findAndToggleChecked, findAndDelete, findAndUpdateName, findById};
